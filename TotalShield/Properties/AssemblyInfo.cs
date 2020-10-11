8f04e388342010de6e0d6565d9f6b7c0429be540
Spinu Daniel <44769115+BateaDjango@users.noreply.github.com>
2020-10-11 12:21:30 +0300

    Add files via upload

---
+++ TotalShield/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TotalShield")]
+[assembly: AssemblyDescription("TotalShield")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Uganda S.R.L")]
+[assembly: AssemblyProduct("TotalShield")]
+[assembly: AssemblyCopyright("Copyright Uganda S.R.L ©  2020")]
+[assembly: AssemblyTrademark("UgandaSec")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f846e12-dba8-47ae-baaa-64321813f9d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
